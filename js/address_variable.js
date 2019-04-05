3b31113fe9f80c598407fef7012b0c252b575b76
nadcdc <nadcdc4@gmail.com>
2019-04-06 02:13:22 +0900

    root_address를 기본적으로 local로 지정하고 배포 상황에 맟게 변경 해주겠다.

--- js/address_variable.js
+++ js/address_variable.js
@@ -1,5 +1,5 @@
 // local
-// var root_address = "http://localhost:8000"
+var root_address = "http://localhost:8000"
 
 // dev
-var root_address = "http://api.localhost:8000"
+// var root_address = "http://api.localhost:8000"
